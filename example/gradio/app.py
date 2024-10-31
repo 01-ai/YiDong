1309e74e65fd770c119ac16d5393bd532641655a
01du <a01@01dudeMacBook-Pro.local>
2024-10-31 14:29:23 +0800

    Delete file

--- example/gradio/app.py
+++ example/gradio/app.py
@@ -1,22 +1,7 @@
-from http import server
 import os
 import logging
 import requests
 import gradio as gr
-from starlette.responses import RedirectResponse
-
-
-def hello(profile: gr.OAuthProfile | None) -> str:
-    # ^ expect a gr.OAuthProfile object as input to get the user's profile
-    # if the user is not logged in, profile will be None
-    if profile is None:
-        return "I don't know you."
-    return f"Hello {profile.name}"
-
-
-# 定义函数执行 POST 请求
-
-
 def get_user_info(profile: gr.OAuthProfile | None) -> str:
     def call_api(user_email):
         url = f"https://api-yidong.lingyiwanwu.com/v1/ops/api_key?user_email={user_email}&user_source=huggingface"
@@ -24,14 +9,8 @@ def get_user_info(profile: gr.OAuthProfile | None) -> str:
         headers = {
             'Authorization': '{}'.format(os.environ.get('auth'))
         }
-        
-        # 发送 POST 请求
         response = requests.post(url, headers=headers)
-        logging.info("打印具体的响应值: {}".format(response.json()["data"]["display_api_key"]))
-        
-        
-        # 返回响应内容
-        '''{"code":0,"message":"Success","data":{"user_id":"07787cc6-9291-11ef-9ec5-ae87c86d3382","api_key_id":"07d53ee8-9291-11ef-9ec5-ae87c86d3382","display_api_key":"tOYnyKqwosGEhtIKNWQgVAol1dhqJ3HN","api_key_name":"","created_at":"2024-10-25T05:21:47.111000Z"}'''
+        logging.info("response:{}".format(response.json()["data"]["display_api_key"]))
         return response.json()["data"]["display_api_key"]
     
 
@@ -39,20 +18,16 @@ def get_user_info(profile: gr.OAuthProfile | None) -> str:
         return "User not logged in."
     
 
-    logging.info(f"用户名: {profile.name}")
+    logging.info(f"username: {profile.name}")
     return call_api(profile.name)
 
-with gr.Blocks() as clip_service:
-    
+with gr.Blocks() as clip_service:   
     with gr.Row():
-        
-
         login_button = gr.LoginButton()
-        user_info_display = gr.Textbox(label="为获取用户密钥，请点击huggingface登陆，第一次登陆时会有完整密钥，请保存。之后会隐藏您的密钥。",interactive=True)  # 用于显示用户信息
+        user_info_display = gr.Textbox(label="In order to get your user key, please click on huggingface login, the first time you login you will have the full key, please save it. After that your key will be hidden.",interactive=True) 
         m1 = gr.Markdown()
         clip_service.load(get_user_info,inputs=None,outputs=user_info_display)
-        # 当用户点击登录按钮并完成 OAuth 登录后，调用 display_user_info 函数
-        # login_button.click(get_user_info, inputs=None, outputs=[user_info_display])
-        logging.info(f"当前变量的值是: {user_info_display}")
+
+        logging.info(f"The value of the current variable is: {user_info_display}")
 
 clip_service.launch(share=True)
